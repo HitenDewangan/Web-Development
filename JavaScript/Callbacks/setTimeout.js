57a51391395ee6fadce13ae468fb6c61c7229adc
Hiten Dewangan <hitendewangan100j@gmail.com>
2025-01-17 14:18:18 +0530

    learning callbacks

---
+++ JavaScript/Callbacks/setTimeout.js
@@ -0,0 +1,18 @@
+// write a function accepting fucntion as a parameter and call it at later time
+
+function add(a, b){
+    console.log(a + b);
+}
+
+function doCalc(a, b, callback){
+    console.log("Before callback");
+
+    // call at later time eg. 1000ms = 1s later
+    setTimeout(() => {
+        callback(a, b);
+    }, 5000);
+
+    console.log("in between callback");
+}
+
+doCalc(3, 4, add);
