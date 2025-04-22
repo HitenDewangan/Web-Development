278ca17ef1f5db5a9b232f13a4f27062f9335afe
HIDED <hitendewangan100j@gmail.com>
2025-04-23 00:46:44 +0530

    JS at client side

--- JavaScript/Apna College/Fetch API/fetch.js
+++ JavaScript/Apna College/Fetch API/fetch.js
@@ -2,16 +2,29 @@ const URL = "https://catfact.ninja/fact";
 const factPara = document.querySelector("#fact");  
 const btn = document.querySelector("#btn");  
 
-const getFacts = async () => {  
-    console.log("getting data .....");  
+// // using async-await
+// const getFacts = async () => {  
+//     console.log("getting data .....");  
 
-    let response = await fetch(URL);  
-    console.log(response); // JSON format  
+//     let response = await fetch(URL);  
+//     console.log(response); // JSON format  
 
-    let data = await response.json();  
-    console.log("API response : ", data.fact);
-    factPara.innerText = data.fact;  
-};  
+//     let data = await response.json();  
+//     console.log("API response : ", data.fact);
+//     factPara.innerText = data.fact;  
+// };  
+
+// using promise-chaining, but async-await is better
+function getFacts() {   
+    fetch(URL)  
+        .then((response) => {
+            return response.json()
+        })  
+        .then((data) => {  
+            console.log("API response : ", data.fact);  
+            factPara.innerText = data.fact;  
+        });  
+};
 
 btn.addEventListener("click", getFacts);
 
