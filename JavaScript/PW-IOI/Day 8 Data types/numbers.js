26c73c0f5c0d70e4606c351001ed84620f95b60f
HIDED <hitendewangan100j@gmail.com>
2025-05-06 19:41:55 +0530

    Day 8 : Data Types & JSON

---
+++ JavaScript/PW-IOI/Day 8 Data types/numbers.js
@@ -0,0 +1,10 @@
+let a = 9007199254740991;
+let b = 9007199254740992;
+let c = 9007199254740993;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(c+1);
+console.log(a === a+1);
+console.log(c === c+1);
