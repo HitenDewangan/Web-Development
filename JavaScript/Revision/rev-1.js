fc2acd2adaf968da3427a1d1590ab497a700ae32
HIDED <hitendewangan100j@gmail.com>
2025-05-22 15:40:42 +0530

    Sign-in page and conditions

---
+++ JavaScript/Revision/rev-1.js
@@ -0,0 +1,10 @@
+'use strict';
+// myVariable = 10; // ReferenceError: myVariable is not defined
+let myVariable; // Correct
+console.log(myVariable);
+
+// initialisation, assignment, declaration
+// int add; //declaration
+// add = 10; // assigmentn
+
+// int x = 10; // initialisation
---
+++ JavaScript/Revision/rev-2.js
@@ -0,0 +1,10 @@
+let a;
+a = 10;
+
+a = 20;
+
+console.log(a);
+
+const b = 10;
+b = 3;
+console.log(b);
