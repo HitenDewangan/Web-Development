59686ec92db7c1a1a6c6073fbda53c67d97e41e8
HIDED <hitendewangan100j@gmail.com>
2025-08-08 14:30:56 +0530

    todo-with-category

---
+++ NEXUS/05 TypeScript/app.ts
@@ -0,0 +1,4 @@
+let a : number = 10;
+let b : number = 20;
+
+// a = "rohit"; // This will cause a type error in TypeScript 
---
+++ NEXUS/05 TypeScript/first.js
@@ -0,0 +1,10 @@
+let age = 20;
+
+age = "twenty"; // This will cause a type error in TypeScript
+
+let obj = {
+    name: "John",
+    age: 30
+}
+
+console.log(obj.len) // This will cause a runtime error since 'len' is not defined in the object
